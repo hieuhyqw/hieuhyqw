d22bc6ec6dc293fd3909c48948ab8c1c033af3ae
hieuhyqw <82858780+hieuhyqw@users.noreply.github.com>
2021-10-20 16:10:34 +0700

    hieuhyqw

---
+++ asset/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        auto: true,
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  $(document).ready(function() {
+    $('#autoWidth-tow').lightSlider({
+    
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth-tow').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+  
+  $(document).ready(function() {
+    $('#autoWidth-three').lightSlider({
+        auto: true,
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth-tow').removeClass('cS-hidden');
+        } 
+    });  
+  });
+  
+  $(document).ready(function() {
+    $('#autoWidth-four').lightSlider({
+    
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth-tow').removeClass('cS-hidden');
+        } 
+    });  
+  });
